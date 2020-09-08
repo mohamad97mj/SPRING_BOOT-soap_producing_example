618f77fa2ef6ee5b0ee72a95de8d8a73a0395498
mohamad <mohamad97mj@gmail.com>
2020-09-08 12:37:46 +0430

    project uploaded to github

---
+++ .idea/.name
@@ -0,0 +1,1 @@
+producing-web-service
---
+++ src/main/java/com/example/producingwebservice/CountryEndpoint.java
@@ -0,0 +1,31 @@
+package com.example.producingwebservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.mohamad97mj.ws.GetCountryResponse;
+import com.mohamad97mj.ws.GetCountryRequest;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://mohamad97mj.com/ws";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
